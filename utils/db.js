e8afb8db057a96fed8268df9ffc9529be17401bc
John ayinde <lolaayinde@gmail.com>
2020-10-01 15:50:32 +0100

    added mongodb url

--- utils/db.js
+++ utils/db.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose')
-const url = 'mongodb://localhost/yelp_camp';
+// const url = 'mongodb://localhost/yelp_camp';
+const db = 'mongodb+srv://campground:campground@cluster0.8croi.mongodb.net/campground?retryWrites=true&w=majority';
+
+
 module.exports = function () {
-    mongoose.connect(url, { useUnifiedTopology: true, useCreateIndex: true, useNewUrlParser: true }).then(() => console.log('DB connected'))
-        .catch((error) => console.log("DB error"))
+    mongoose.connect(db, { useUnifiedTopology: true, useCreateIndex: true, useNewUrlParser: true }).then(() => console.log('DB connected'))
+        .catch((error) => console.log("DB error", error))
 
 
 }
