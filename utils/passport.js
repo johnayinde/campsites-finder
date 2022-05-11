f89a8f9166279264d26d64e0f2dfc6b55f351b91
Ayinde John <lolaayinde@gmail.com>
2022-05-11 03:49:06 +0100

    updated screet key

--- utils/passport.js
+++ utils/passport.js
@@ -1,21 +1,24 @@
-const localStrategy = require('passport-local'),
-    passport = require('passport'),
-    User = require('../models/user');
+const localStrategy = require("passport-local"),
+	passport = require("passport"),
+	User = require("../models/user");
 
 module.exports = function (app) {
-    /**
-    * ============
-    *  PASSPORT CONFIGS
-    * ===========
-    */
+	/**
+	 * ============
+	 *  PASSPORT CONFIGS
+	 * ===========
+	 */
 
-    app.use(require('cookie-session')({
-        secret: process.env.MY_SECRET, resave: false, saveUninitialized: false
-    }));
-    app.use(passport.initialize());
-    app.use(passport.session());
-    passport.use(new localStrategy(User.authenticate()));
-    passport.serializeUser(User.serializeUser());
-    passport.deserializeUser(User.deserializeUser());
-
-}
+	app.use(
+		require("cookie-session")({
+			secret: "my secret",
+			resave: false,
+			saveUninitialized: false,
+		})
+	);
+	app.use(passport.initialize());
+	app.use(passport.session());
+	passport.use(new localStrategy(User.authenticate()));
+	passport.serializeUser(User.serializeUser());
+	passport.deserializeUser(User.deserializeUser());
+};
