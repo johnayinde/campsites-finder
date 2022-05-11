37905e08fd3513cc0197085606bcbb523784bc99
Ayinde John <lolaayinde@gmail.com>
2022-05-11 04:32:55 +0100

    update

--- utils/db.js
+++ utils/db.js
@@ -3,14 +3,11 @@ const mongoose = require("mongoose");
 const DB = process.env.DATABASE_URL || "mongodb://localhost/yelp_camp";
 module.exports = function () {
 	mongoose
-		.connect(
-			"mongodb+srv://campground:campground@cluster0.8croi.mongodb.net/campground?retryWrites=true&w=majority",
-			{
-				useUnifiedTopology: true,
-				useCreateIndex: true,
-				useNewUrlParser: true,
-			}
-		)
+		.connect(DB, {
+			useUnifiedTopology: true,
+			useCreateIndex: true,
+			useNewUrlParser: true,
+		})
 		.then(() => console.log("DB connected"))
 		.catch((error) => console.log("DB error", error));
 };
